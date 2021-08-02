d7ad740720b168f6c35a8817e0991ed66b82510f
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2021-08-02 04:18:52 -0700

    Don't delete both dll and so in BizHawk, only one or the other can be updated at a time

--- scripts/.copy_bizhawk_after_build.sh
+++ scripts/.copy_bizhawk_after_build.sh
@@ -2,10 +2,8 @@
 cd "$(dirname "$0")/../libgambatte"
 if [ -e "../../../BizHawk.sln" ]; then
 	echo "copying output for BizHawk..."
-	rm -f ../../../Assets/dll/libgambatte.*
 	cp -v -t ../../../Assets/dll libgambatte.dll libgambatte.so
 	if [ -e "../../../output/dll" ]; then
-		rm -f ../../../output/dll/libgambatte.*
-		cp -v -t ../../../output/dll libgambatte.*
+		cp -v -t ../../../output/dll libgambatte.dll libgambatte.so
 	fi
 fi
