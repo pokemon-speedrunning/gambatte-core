abf8f1c38393939d8643b166eab2c90ebcfcecaa
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2022-03-25 19:52:15 -0700

    add in missing savestate member to statesaver

--- libgambatte/src/statesaver.cpp
+++ libgambatte/src/statesaver.cpp
@@ -383,6 +383,7 @@ SaverList::SaverList()
 	{ static char const label[] = { r,t,c,l,h,     NUL }; ADD(rtc.latchH); }
 	{ static char const label[] = { r,t,c,l,m,     NUL }; ADD(rtc.latchM); }
 	{ static char const label[] = { r,t,c,l,s,     NUL }; ADD(rtc.latchS); }
+	{ static char const label[] = { i,r,t,r,i,g,   NUL }; ADD(ir.irTrigger); }
 	{ static char const label[] = { i,r,t,s,i,g,   NUL }; ADD(ir.thisGbIrSignal); }
 	{ static char const label[] = { i,r,l,s,i,g,   NUL }; ADD(ir.linkedGbIrSignal); }
 	{ static char const label[] = { h,NO3,h,a,l,t,t, NUL }; ADD(huc3.haltTime); }
